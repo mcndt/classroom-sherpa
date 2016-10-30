63aa45dd864a1d8f955c7edd2972a46e65c687b3
mcndt <maximioCNDT@gmail.com>
2016-10-30 21:01:31 +0100

    First working version
    needs some visual work.

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -2,33 +2,35 @@ var rocky = require('rocky');
 var schedule = require('./schedule.json');
 
 rocky.on('draw', function(event) {
+  
+  // setup
   var context = event.context;
   context.clearRect(0, 0, context.canvas.clientWidth, context.canvas.clientHeight);
+  
   var w = context.canvas.unobstructedWidth;
   var h = context.canvas.unobstructedHeight;
-  var date = new Date("2016-10-28T12:41:00");
+  var date = new Date("2016-10-28T13:41:00");
+  var course = "*";
+  var room = "*";
+  var tc = toTimecode(date);
   
-  
+  // draw time
   context.fillStyle = "white";
   context.textAlign = "center";
   context.font = "28px light numbers Leco-numbers";
   context.fillText(date.toLocaleTimeString(), w * (1/2), h * (1/3), w);
   
   
-  var course = "*";
-  var room = "*";
-  var tc = toTimecode(date).parseInt();
-
-  for (var i = 0; i < schedule[date.getDay()]; i++) {
+  // draw sherpa
+  for (var i = 0; i < schedule[date.getDay()].length; i++) {
     if (tc >= schedule[date.getDay()][i].start && tc < schedule[date.getDay()][i].end) {
       course = schedule[date.getDay()][i].class;
       room =   schedule[date.getDay()][i].room;
-      return;
     }
   }
   
   context.font = "18px bold Gothic";
-  context.fillText( "course + room + tc", w * (1/2), h * (2/3), w);
+  context.fillText( course + " / " + room, w * (1/2), h * (2/3), w);
 });
 
 rocky.on('minutechange', function(event) {

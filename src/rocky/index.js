30864ae03f15033fd4cabfe9bb50a04090bcc04e
mcndt <maximioCNDT@gmail.com>
2016-10-30 22:27:36 +0100

    v1.0
    first working version!

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -9,17 +9,22 @@ rocky.on('draw', function(event) {
   
   var w = context.canvas.unobstructedWidth;
   var h = context.canvas.unobstructedHeight;
-  var date = new Date("2016-10-28T13:41:00");
-  var course = "*";
-  var room = "*";
+//  var date = new Date("2016-10-28T13:41:00");
+  var date = new Date();
+  var course = " -";
+  var room = " -";
   var tc = toTimecode(date);
   
   // draw time
   context.fillStyle = "white";
   context.textAlign = "center";
-  context.font = "28px light numbers Leco-numbers";
-  context.fillText(date.toLocaleTimeString(), w * (1/2), h * (1/3), w);
-  
+  context.font = "42px bold numbers Leco-numbers";
+  context.fillText(date.toLocaleTimeString(undefined, {hour: '2-digit'}) + ":" +
+                   date.toLocaleTimeString(undefined, {minute: '2-digit'}),
+                   (w/2), (h/4), w);
+  context.font = "14px Gothic";
+  context.fillStyle = "#b3b3b3";
+  context.fillText(date.getDate() + "/" + (date.getMonth() + 1), (w/2), ((h/4)+45), w);
   
   // draw sherpa
   for (var i = 0; i < schedule[date.getDay()].length; i++) {
@@ -29,8 +34,22 @@ rocky.on('draw', function(event) {
     }
   }
   
-  context.font = "18px bold Gothic";
-  context.fillText( course + " / " + room, w * (1/2), h * (2/3), w);
+  context.font = "24px bold Gothic";
+  context.fillStyle = "#ff6600";
+  context.textAlign = "left";
+  context.fillText( course, 9, (h-33), w);
+  context.textAlign = "right";
+  context.fillText( room, (w-9), (h-33), w);
+  
+  context.strokeStyle = "#ff6600";
+  context.lineWidth = 2;
+  context.beginPath();
+  context.moveTo(6, (h-36));
+  context.lineTo((w-6), (h-36));
+  context.stroke();
+  
+  
+  
 });
 
 rocky.on('minutechange', function(event) {
